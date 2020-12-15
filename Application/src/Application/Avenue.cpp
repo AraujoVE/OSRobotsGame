b8f8d7b5518e6951c40c6ea77851ae8188629616
Lucas <lucasmfromero@gmail.com>
2020-12-15 00:59:34 -0300

    Created Avenue class

---
+++ Application/src/Application/Avenue.cpp
@@ -0,0 +1,60 @@
+#include "header/Avenue.hpp"
+
+Avenue::Avenue (int &attr) {
+    sem_init(&empty, 1, 0);
+    sem_init(&full, 1, FULL_N);
+    pthread_mutex_init(&mutex, NULL);
+    this->attr = attr;
+}
+
+Avenue::~Avenue () {
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    pthread_mutex_destroy(&mutex);
+}
+
+void Avenue::producer (int value) {
+    sem_wait(&empty);
+    pthread_mutex_lock(&mutex);
+
+    items.push(value);
+
+    pthread_mutex_unlock(&mutex);
+    sem_wait(&full);
+}
+
+void Avenue::consumer () {
+    while (true) {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+        
+        attr += items.front();
+        items.pop();
+
+        if (attr < 0)
+            attr = 0;
+
+        pthread_mutex_unlock(&mutex);
+        sem_wait(&empty);
+    }
+}
+
+void Avenue::up () {
+    pthread_mutex_unlock(&mutex);
+}
+
+void Avenue::down () {
+    pthread_mutex_lock(&mutex);
+}
+
+int Avenue::getValue () {
+    int value;
+
+    pthread_mutex_lock(&mutex);
+    
+    value = attr;
+    
+    pthread_mutex_unlock(&mutex);
+
+    return value;
+}
---
+++ Application/src/Application/header/Avenue.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <queue>
+
+#define FULL_N 1000
+
+class Avenue {
+    private:
+        sem_t empty, full;
+        pthread_mutex_t mutex;
+        int attr;
+        std::queue<int> items;
+
+    public:
+        Avenue(int &attr);
+        ~Avenue();
+
+        void producer (int value);
+        void consumer ();
+
+        void up();
+        void down();
+
+        int getValue ();
+};
