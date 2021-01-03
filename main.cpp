565d5dda7f4d95dfaa0feb63c3835388975cedb2
Haltz01 <pedro-x-guerra@hotmail.com>
2021-01-03 11:48:12 -0300

    Adding 3 selection methods

--- main.cpp
+++ main.cpp
@@ -14,11 +14,12 @@
 */
 #include<stdlib.h>  /* srand, rand */
 #include<iostream>
+#include <vector>
 // #include<cstring>
 // #include<unistd.h>
 // #include <thread>   /* std::this_thread::sleep_for */
 // #include <chrono>   /* std::chrono::seconds */
-// #include <GL/glut.h>
+// #include <GL/glut.h> /* simple GUI + keyboard usage */
 // #include <loguru.cpp> // logging lib -> https://emilk.github.io/loguru/index.html#loguru
 
 #define NB_PARAMETERS 10 // number of parameters that will be adjusted
@@ -27,11 +28,15 @@
 
 #define MAX_PARAM_VALUE 1 // max value a parameter can reach
 
+enum selectionMethods { TOURNAMENT, ELITISM, ROULETTE };
+selectionMethods selectionMethod = ELITISM;
+
 double population[POPULATION_SIZE][NB_PARAMETERS];
 double fitness[NB_PARAMETERS];
 double maxFitness = 0, minFitness = 2000000000;
 int maxFitIndex = -1;
 double mutationRate = MAX_MUTATION_RATE;
+
 
 // 1st step: initialize population
 void initializePop() {
@@ -43,6 +48,16 @@ void initializePop() {
     }
 
     return;
+}
+
+double * normalizeFitness() {
+    double normalizedFitness[POPULATION_SIZE];
+
+    for (int i = 0; i < POPULATION_SIZE; i++) {
+        normalizedFitness[i] = (fitness[i]-minFitness)/(maxFitness-minFitness);
+    }
+
+    return normalizedFitness;
 }
 
 // 2nd step: evaluate population (calculate fitness)
@@ -65,6 +80,96 @@ void evaluatePop() {
     }
 }
 
+void elitism() {
+    for (int i = 0; i < POPULATION_SIZE; i++) {
+        if (i == maxFitIndex) // keeping the best individual (no changes are made to it)
+            continue;
+        
+        // crossover
+        for (int j = 0; j < NB_PARAMETERS; j++) {
+            population[i][j] = (population[i][j] + population[maxFitIndex][j])/2.0;
+        }
+    }
+
+    return;
+}
+
+void tournament() {
+    double oldPopulation[POPULATION_SIZE][NB_PARAMETERS];
+    int parentIndex[2]; 
+    
+    // copying last population (new one will be different)
+    for (int i = 0; i < POPULATION_SIZE; i++) {
+        for (int j = 0; j < NB_PARAMETERS; j++) {
+            oldPopulation[i][j] = population[i][j];
+        }
+    }
+
+    for (int i = 0; i < POPULATION_SIZE; i++) {
+        if (i == maxFitIndex)
+            continue;
+        
+        // chossing parents for new individual
+        for (int j = 0; j < 2; j++) {
+            int indexIndA = rand() % POPULATION_SIZE; // indv 1 that will "fight" to be parent
+            int indexIndB = rand() % POPULATION_SIZE; // indv 2 that will "fight" to be parent
+
+            parentIndex[j] = (fitness[indexIndA] > fitness[indexIndB]) ? indexIndA : indexIndB;
+        }
+
+        // crossover
+        for (int j = 0; j < NB_PARAMETERS; j++) {
+            population[i][j] = (oldPopulation[parentIndex[0]][j] + oldPopulation[parentIndex[1]][j])/2.0;
+        }
+    }
+
+    return;
+}
+
+void roulette() {
+    double oldPopulation[POPULATION_SIZE][NB_PARAMETERS];
+    double standardizedFitness[POPULATION_SIZE];
+    int parentIndex[2], rNb; 
+    double probSum = 0.0, partialSum = 0.0;
+    
+    // copying last population (new one will be different)
+    for (int i = 0; i < POPULATION_SIZE; i++) {
+        for (int j = 0; j < NB_PARAMETERS; j++) {
+            oldPopulation[i][j] = population[i][j];
+        }
+    }
+
+    // Standardize fitness (set probabilites that add up to 100%)
+    for (int i = 0; i < POPULATION_SIZE; i++)
+        probSum += fitness[i];
+    for (int i = 0; i < POPULATION_SIZE; i++)
+        standardizedFitness[i] = fitness[i]/probSum;
+    
+    // Chosing new parents for each individual
+    for (int i = 0; i < POPULATION_SIZE; i++) {
+        if (i == maxFitIndex) // preserves best individual
+            continue;
+        
+        for (int k = 0; k < 2; k++) { // chosing 2 parents
+            rNb = ((double) rand() / RAND_MAX); // rand between 0 and 1
+            partialSum = 0.0;
+
+            for (int j = 0; j < POPULATION_SIZE; j++) { // randomly chosing and individual according to its fitness (+fitness = +probabity)
+                partialSum += fitness[j];
+                if (partialSum >= rNb) {
+                    parentIndex[k] = j; // new parent at index j
+                    break;
+                }
+            }
+        }
+
+        // crossover
+        for (int j = 0; j < NB_PARAMETERS; j++) {
+            population[i][j] = (oldPopulation[parentIndex[0]][j] + oldPopulation[parentIndex[1]][j])/2.0;
+        }
+    }
+}
+
 // 3rd step: selection + mutation and crossover
 void selection() { // tournament, elitism, roulette...
     /* 
@@ -74,25 +179,27 @@ void selection() { // tournament, elitism, roulette...
 
     Stochastic Uniform (Fitness Proportionate) -> PROB THE BEST?
     */
-    double normalizedFitness[POPULATION_SIZE];
 
-    for (int i = 0; i < POPULATION_SIZE; i++) {
-        normalizedFitness[i] = (fitness[i]-minFitness)/(maxFitness-minFitness);
-    }
-
-    for (int i = 0; i < POPULATION_SIZE; i++) {
-        if (i == maxFitIndex) // keeping the best individual (no changes are made to it)
-            continue;
-
-        // Selection method???
-
-        // Crossover
-        // ...
+        // Selection method (+ crossover)
+        switch (selectionMethod) {
+            case TOURNAMENT:
+                tournament();
+                break;
+            case ELITISM:
+                elitism();
+                break;
+            case ROULETTE:
+                roulette();
+                break;
+        }
 
         // Mutation
         for (int i = 0; i < POPULATION_SIZE; i++) { 
+            if (i == maxFitIndex)
+                continue; // don't mutate the best from last generation
+
             for (int j = 0; j < NB_PARAMETERS; j++) {
-                population[i][j] += ((double) ((double) (rand() % MAX_PARAM_VALUE) - MAX_PARAM_VALUE/2) * (mutationRate));
+                population[i][j] += (((double) (rand() % MAX_PARAM_VALUE) - MAX_PARAM_VALUE/2.0) * (mutationRate));
             }
 
             if (population[i][j] < 0)
@@ -119,5 +226,6 @@ int main(int argc, char * argv[]) {
         std::cout << "\n==== Generation " << generationIndex << "====\n";
         evaluatePop();
         selection();
+        generationIndex++;
     }
 }
